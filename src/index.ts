65a4219e0f4300801d7c47c5a668bcdb93461e19
shabithkms <shabithkms2035@gmail.com>
2022-12-31 18:00:55 +0530

    created simple todo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,64 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: String;
+  title: String;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-list');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach((task) => {
+  addListItem(task);
+});
+
+// Catch form submit event
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = input.value;
+  if (title === '' || title === null) return;
+  const id = uuidV4();
+  const newTask: Task = {
+    id,
+    title,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+  saveTasks();
+
+  addListItem(newTask);
+  input.value = '';
+});
+
+function addListItem(task: Task): boolean {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+  return true;
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  let taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
